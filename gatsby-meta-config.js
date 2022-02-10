0c1f9fb9485c7ac8cc5580b04361dcef81c6e7d4
seungjun-dev <seungjun91@naver.com>
2022-02-10 19:53:18 +0900

    토이프로젝트
    quit
    exit
    /

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -49,7 +49,7 @@ module.exports = {
       },
       {
         date: '2017.10 ~ 2019.08',
-        activity: '닐슨코리아 프론트엔드 개발'
+        activity: '닐슨코리아 IT운영'
       },
       {
         date: '2018.12',
@@ -77,18 +77,30 @@ module.exports = {
           demo: '',
         },
       },
-      // ========================================================
+      // ========================================================~
       // ========================================================
       {
-        title: '개발 블로그 테마 개발',
+        title: 'Best Pick(이상형 월드컵)',
         description:
-          '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-        techStack: ['gatsby', 'react'],
-        thumbnailUrl: 'blog.png',
+          '이상형 월드컵입니다. 최근 심리테스트 등 캐주얼한 컨텐츠를 담고 있는 링크들이 많은 것 같아 만들어보았습니다.',
+        techStack: ['Vanilla JS'],
+        thumbnailUrl: 'project_bestpick.png',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
+          //post: '/gatsby-starter-zoomkoding-introduction',
+          github: 'https://github.com/seungjun-dev/best_pick',
+          demo: 'https://seungjun.me/best_pick',
+        },
+      },
+      {
+        title: '숫자 야구 게임',
+        description:
+          '어렸을때 즐겨하던 숫자 야구 게임입니다.',
+        techStack: ['Vanilla JS'],
+        thumbnailUrl: 'project_baseball.png',
+        links: {
+          //post: '/gatsby-starter-zoomkoding-introduction',
+          github: 'https://github.com/seungjun-dev/baseball',
+          demo: 'https://seungjun.me/baseball',
         },
       },
     ],
